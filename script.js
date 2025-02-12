4b4b73f03a9889dd7109038711e4d16e73503d12
BiniElla <mrdea22@src.pshs.edu.ph>
2025-02-12 15:45:49 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function ExponentialPopGrowth() {
+    let InitialPopulation = parseFloat(prompt('Enter the initial populaiton:'));
+    let RateOfGrowth = parseFloat(prompt('Enter the rate of growth:'));
+    let TimeInHours = parseFloat(prompt('Enter the time:'));
+
+    let FinalPopulation = Math.round(InitialPopulation * Math.pow(Math.E, (RateOfGrowth*TimeInHours)));
+
+    let location = prompt('Enter the location:');
+    let MonsterName = prompt('Enter the name of the Monster:');
+
+    let NameOfMonsterAndLocation = location.concat("", MonsterName).toUpperCase();
+
+    document.getElementById("result").innerHTML="After " + TimeInHours + " Hour/s, the population of " + NameOfMonsterAndLocation + " has risen to " + FinalPopulation;
+
+}
